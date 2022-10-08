c80f2b9547bab616d49f35b52a142e443235f841
Yann BELLOT <ybellot@e3r7p2.clusters.42paris.fr>
2022-10-08 19:21:41 +0200

    parametrage vscode cpp, ex00

--- ex00/megaphone.cpp
+++ ex00/megaphone.cpp
@@ -1,7 +1,25 @@
 #include <iostream>
+#include <string>
 
-int	main()
+int	main(int argc, char **argv)
 {
-	std::cout << "Hello World!\n";
+	std::string msg;
+	int msglen;
+
+	if (argc == 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+		return (0);
+	}
+	for (int i = 1; i < argc; i++)
+	{
+		msg = argv[i];
+		msglen = msg.length();
+		for(int j = 0; j < msglen; j++)
+		{
+			std::cout << (char)std::toupper(msg[j]);
+		}	
+	}
+	std::cout << '\n';
 	return (0);
 }
