09d393b61c055a59ef2a410367b71059049f6823
Yann BELLOT <ybellot@e3r2p2.clusters.42paris.fr>
2022-11-26 14:56:43 +0100

    c06 ex 02 ok

--- c06/ex02/main.cpp
+++ c06/ex02/main.cpp
@@ -19,12 +19,12 @@ Base * generate(void)
 		A* pt_A = new A;
 		return (pt_A);	
 	}
-	if (random % 3 == 1)
+	else if (random % 3 == 1)
 	{
 		B* pt_B = new B;
 		return (pt_B);	
 	}
-	if (random % 3 == 2)
+	else
 	{
 		C* pt_C = new C;
 		return (pt_C);	
@@ -33,18 +33,48 @@ Base * generate(void)
 
 void identify(Base* p)
 {
-
+	if (dynamic_cast<A*>(p) != NULL)
+	{
+		std::cout << "Type is A" << std::endl;
+	}
+	if (dynamic_cast<B*>(p) != NULL)
+	{
+		std::cout << "Type is B" << std::endl;
+	}
+	if (dynamic_cast<C*>(p) != NULL)
+	{
+		std::cout << "Type is C" << std::endl;
+	}
 }
 
 void identify(Base& p)
 {
-
+	Base& check = p;
+	try
+	{
+		check = dynamic_cast<A&>(p);
+		std::cout << "Type is A" << std::endl;
+	}
+	catch(const std::exception& e)
+	{
+		try
+		{
+			check = dynamic_cast<B&>(p);
+			std::cout << "Type is B" << std::endl;
+		}
+		catch(const std::exception& e)
+		{
+			std::cout << "Type is C" << std::endl;
+		}
+	}
 }
-
-
 
 int main(void)
 {
-	Base* pt_random_base = generate();
+	Base* pt_random = generate();
+
+	identify(pt_random);
+	identify(*pt_random);
+
 	return (0);
 }
