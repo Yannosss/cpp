2ed0d965a305c32f80424d5b680f6d85c79f24e7
Yann BELLOT <ybellot@e3r4p14.clusters.42paris.fr>
2022-11-30 17:15:03 +0100

    c07 check pour push

--- c07/ex02/mainperso.cpp
+++ c07/ex02/mainperso.cpp
@@ -12,14 +12,14 @@ int main()
 		std::cout << "check a: " << a[1] << std::endl;
 		std::cout << "check a: " << a[2] << std::endl;
 
-		std::cout << "--------" << a[0] << std::endl;
+		std::cout << "--------" << std::endl;
 		std::cout << "-- check copy operator =" << std::endl;
 		Array<int> b = a;
 		std::cout << "check b: " << b[0] << std::endl;
 		std::cout << "check b: " << b[1] << std::endl;
 		std::cout << "check b: " << b[2] << std::endl;
 
-		std::cout << "--------" << a[0] << std::endl;
+		std::cout << "--------" << std::endl;
 		std::cout << "-- check modification copy" << std::endl;
 		b.get_element()[0] = 7;
 		b.get_element()[1] = 8;
@@ -31,13 +31,13 @@ int main()
 		std::cout << "check b: " << b[1] << std::endl;
 		std::cout << "check b: " << b[2] << std::endl;
 
-		std::cout << "--------" << a[0] << std::endl;
+		std::cout << "--------"<< std::endl;
 		std::cout << "-- check recopy" << std::endl;
 		Array<int> c(b);
 		std::cout << "check c: " << c[0] << std::endl;
 		std::cout << "check c: " << c[1] << std::endl;
 		std::cout << "check c: " << c[2] << std::endl;
-		std::cout << "--------" << a[0] << std::endl;
+		std::cout << "--------" << std::endl;
 		std::cout << "-- check modification copy" << std::endl;
 		c.get_element()[0] = 10;
 		c.get_element()[1] = 11;
@@ -54,9 +54,7 @@ int main()
 		std::cout << "size a: " << b.size() << std::endl;
 		std::cout << "size a: " << c.size() << std::endl;
 
-
-
-		std::cout << "--------" << a[0] << std::endl;
+		std::cout << "--------" << std::endl;
 		std::cout << "-- check access [] outof range (exception)" << std::endl;
 		std::cout << a[3] << std::endl;
 
